9652642f0cc0b1437034cde2d3775187989cdf00
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-17 00:36:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
 def register():
     return render_template('register.html')
 
+@app.route('/login')
 def login():
     return render_template('login.html')
-
-def serve_static(filename):
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
 if __name__ == '__main__':
     app.run()
