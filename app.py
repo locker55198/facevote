b97f48646d277bd0ebb710447b1d38e084c3d3f3
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-16 22:52:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vote.jpg')
+
 if __name__ == '__main__':
     app.run()
