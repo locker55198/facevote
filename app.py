48f8879f458e472933f8ade766a5d931c1266860
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-17 15:46:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
+import math
+import time
+import cvzone
+from ultralytics import YOLO
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def detect_objects():
+    confidence = 0.6
+    cap = cv2.VideoCapture(0)  # For Webcam
+    cap.set(3, 640)
+    cap.set(4, 480)
+    
+    model = YOLO("models/best.pt")
+
+    classNames = ["fake", "real"]
+
+    prev_frame_time = 0
+    new_frame_time = 0
+
+    while True:
+        new_frame_time = time.time()
+        success, img = cap.read()
+        results = model(img, stream=True, verbose=False)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # Bounding Box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                w, h = x2 - x1, y2 - y1
+                # Confidence
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                # Class Name
+                cls = int(box.cls[0])
+                if conf > confidence:
+                    if classNames[cls] == 'real':
+                        color = (0, 255, 0)
+                    else:
+                        color = (0, 0, 255)
+                    cvzone.cornerRect(img, (x1, y1, w, h), colorC=color, colorR=color)
+                    cvzone.putTextRect(img, f'{classNames[cls].upper()} {int(conf*100)}%',
+                                       (max(0, x1), max(35, y1)), scale=2, thickness=4, colorR=color, colorB=color)
+
+        fps = 1 / (new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
+        print(fps)
+
+        ret, jpeg = cv2.imencode('.jpg', img)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    cap.release()
 
 @app.route('/register')
 def register():
@@ -13,6 +66,10 @@ def register():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(detect_objects(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/vote')
 def vote():
