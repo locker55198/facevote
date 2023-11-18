706411067b203720b8d6c7bc25227b249171969f
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-18 12:30:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/register')
 def register():
     return render_template('register.html')
