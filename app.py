db1b54a4ca54ea4e78d743a50c0b0f867e9d4bcd
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-18 12:26:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/vote')
+def vote():
+    return render_template('vote.html')
 
 
 if __name__ == '__main__':
