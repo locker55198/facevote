1d6afd6a5fb3bdcba7a89d6f6ffe55fa86651691
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-18 23:26:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import (Flask, redirect, render_template, request, jsonify, send_from_directory, url_for)
+
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -16,17 +15,23 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('name')
+        image = request.files['image'].read()  # 獲取上傳的圖片文件並讀取其內容
+        save_to_database(name, image)  # 假設有一個名為save_to_database的函數來處理數據保存
+        return jsonify({'message': 'Registration successful'})
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/vote')
+def vote():
+    return render_template('vote.html')
 
 
 if __name__ == '__main__':
-   app.run()
+    app.debug = True
+    app.run()
