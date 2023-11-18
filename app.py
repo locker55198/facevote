c88b9f5f32672ffbb48392866e4ee469161b3b67
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-18 18:22:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import (Flask, redirect, render_template, request, response, jsonify, send_from_directory, url_for)
+from flask import (Flask, redirect, render_template, request, jsonify, send_from_directory, url_for)
 
 app = Flask(__name__)
 
