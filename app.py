84b6e993f2055fb4386a5a4f3659509306c190d1
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-18 19:10:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import (Flask, redirect, render_template, request, jsonify, send_from_directory, url_for)
-from database import save_to_database
+from conn import save_to_database
 
 app = Flask(__name__)
 
@@ -18,9 +18,9 @@ def favicon():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form.get('username')
+        username = request.form.get('name')
         image = request.files['image'].read()  # 獲取上傳的圖片文件並讀取其內容
-        save_to_database(username, image)  # 假設有一個名為save_to_database的函數來處理數據保存
+        save_to_database(name, image)  # 假設有一個名為save_to_database的函數來處理數據保存
         return jsonify({'message': 'Registration successful'})
 
 @app.route('/login')
