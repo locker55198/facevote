f0d6c1bc54322ebeaa7445617461ee222a90a0e2
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-20 11:37:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import os
+
 from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for
 import cv2
 import math
 import time
 import cvzone
+from waitress import serve
 from PIL import Image
 import numpy as np
 import json
-from waitress import serve
+import onnxruntime as ort
+from ultralytics import YOLO
 
 app = Flask(__name__)
-
-class YOLO:
-    def __init__(self, model_path):
-        # 初始化YOLO模型
-        # ...
 
 def detect_objects():
     confidence = 0.6
